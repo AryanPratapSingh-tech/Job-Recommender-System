0b1384c1a378f414a221799c30344af2be9eda28
AryanPratapSingh-tech <aryan7800singhpc@gmail.com>
2026-01-15 12:51:27 +0530

    Important Methods Created

---
+++ src/job_api.py
@@ -0,0 +1,43 @@
+from apify_client import ApifyClient
+import os
+from dotenv import load_dotenv 
+
+load_dotenv()
+
+apify_client = ApifyClient(os.getenv("APIFY_API_TOKEN"))
+
+
+
+#Method to fetch linkedIn Jobs
+def fetch_LinkedIn_Jobs(search_query, location = "India", rows=60):
+
+  run_input = {
+    "title" : search_query,
+    "location" : location,
+    "rows" : rows,
+    "proxy": {
+      "useApifyProxy": True,
+      "apifyProxyGroups": ["RESIDENTIAL"]
+    }
+  }
+
+  run = apify_client.actor("BHzefUZlZRKWxkTck").call(run_input=run_input)
+  jobs = list(apify_client.dataset(run["defaultDatasetId"]).iterate_items())
+  return jobs
+  
+
+
+
+def fetch_Naukari_Jobs(search_query, location="India", rows=60):
+
+  run_input = {
+    "keyword": search_query,
+    "maxJobs": 60,
+    "freshness": "all",
+    "sortBy": "relevance",
+    "experience" : "all",
+  }
+
+  run = apify_client.actor("wsrn5gy5C4EDeYCcD").call(run_input=run_input)
+  jobs = list(apify_client.dataset(run["defaultDatasetId"]).iterate_items())
+  return jobs
---
+++ src/utility_meth.py
@@ -0,0 +1,57 @@
+import fitz 
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+#from openai.error import APIConnectionError, APIResponseValidationError, AuthenticationError
+
+#Loading Environment
+load_dotenv()
+#Loading OpenAI API keys from .env
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+
+if not OPENAI_API_KEY:
+  raise EnvironmentError("OPENAI_API_KEY is not set")
+
+client = OpenAI(api_key=OPENAI_API_KEY)
+
+def extract_text_from_pdf(pdf_path):
+  """
+  extracting text from PDF file.
+  
+  Arguments:
+          pdf_path (str): The Path to the pdf file.
+          
+  Returns:
+         str: The extracted text.
+      
+  """
+  document = fitz.open(stream=pdf_path.read(), filetype="pdf")
+  text =""
+
+  for page in document:
+    text += page.get_text()
+  return text
+
+
+def ask_GPT(prompt, max_tokens=500):
+  response = client.chat.completions.create(
+  model="gpt-4o",
+  messages=[
+    { "role": "user", "content": prompt }
+  ],
+  temperature=0.5,
+  max_tokens=max_tokens
+    )  
+  return response.choices[0].message.content.strip()
+
+
+
+
+
+
+
+
+
+
+
